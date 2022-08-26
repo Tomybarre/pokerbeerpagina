f64358f190afb19f0e5778990b5dee5ff89fd01b
tomybarre <tomybarre@gmail.com>
2022-08-25 23:40:16 -0300

    8 guardado

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -5,10 +5,10 @@ class Producto {
         this.precio = precio
         this.stock = stock
     }
-    contendioNeto(){
-        let contenidoNeto = this.contenidoNeto 
-        return contenidoNeto +"ml"
-         
+    contendioNeto() {
+        let contenidoNeto = this.contenidoNeto
+        return contenidoNeto + "ml"
+
 
     }
     precioConIVA() {
@@ -20,27 +20,29 @@ class Producto {
     }
 }
 
-const prod1 = new Producto("cerveza blonde ale",  500 , 120, 200 )
-const prod2 = new Producto("cerveza ipa",500, 150, 400)
-const prod3 = new Producto("cerveza blonde ale maracuya",500, 130, 300)
-const prod4 = new Producto("barril blonde ale",30000, 3000, 20)
-const prod5 = new Producto("barril ipa",30000, 3500, 20)
-const prod6 = new Producto("barril blonde ale maracuya",3250, 3000, 30)
+const prod1 = new Producto("cerveza blonde ale", 500, 120, 200)
+const prod2 = new Producto("cerveza ipa", 500, 150, 400)
+const prod3 = new Producto("cerveza blonde ale maracuya", 500, 130, 300)
+const prod4 = new Producto("barril blonde ale", 30000, 3000, 20)
+const prod5 = new Producto("barril ipa", 30000, 3500, 20)
+const prod6 = new Producto("barril blonde ale maracuya", 3250, 3000, 30)
 
 
 const IVA = 1.22
 
 
 function nuevoProducto() {
-    nombre = prompt("Ingrese el nombre del producto")
-    contenidoNeto = prompt("Ingrese el contenidoNeto")
-    precio = prompt("Ingrese el precio")
-    stock = prompt("Ingrese el stock")
-    const prodNuevo = new Producto(nombre, contenidoNeto, precio, stock)
-    console.log(prodNuevo)
+    nombre = document.getElementById("nombre").value
+    contenidoNeto = document.getElementById("contenido neto").value
+    precio = document.getElementById("precio").value
+    stock = document.getElementById("stock").value
+    guardarDatos()
+    recuperarDatos()
+    alert("👍Producto guardado")
+
 }
 
-const productos = [] 
+const productos = []
 
 
 
@@ -53,49 +55,55 @@ function generadorAutomatico() {
     productos.push(prod4)
     productos.push(prod5)
     productos.push(prod6)
-    productos.push(new Producto("cerveza amber ale",500, 130, 200))
-    productos.push(new Producto("barril amber ale",30000, 3200, 20))
+    productos.push(new Producto("cerveza amber ale", 500, 130, 200))
+    productos.push(new Producto("barril amber ale", 30000, 3200, 20))
 
 }
 
 generadorAutomatico()
 
-function encontrarBarriles(){
-    let barriles = productos.filter(producto => producto.nombre.includes("barril"))
-      console.log(barriles)
-}
+
 
 function buscarProducto() {
-    let prod = prompt("Ingresa el producto a buscar:")
-      
-        const resultado = productos.find(producto => producto.nombre === prod.toLowerCase())
-         console.log(resultado)
-         
+
+        prod = document.getElementById("Buscar Productos").value
+    const resultado = productos.find(producto => producto.nombre === prod.toLowerCase())
+   document.getElementById("productoEncontrado").innerHTML = resultado.nombre + " " + resultado.contenidoNeto + " " + resultado.precio + " " + resultado.stock
+
 }
 
-function agregarProductos() {
-    let nombre = prompt("Ingresa el nombre del Producto:")
-    let contenidoNeto = parseInt(prompt("Ingrese el contenido:"))
-    let precio = parseInt(prompt("Ingrese precio:"))
-    let stock = parseInt(prompt("Ingresa el stock:"))
-        productos.push(new Producto(nombre, contenidoNeto, precio, stock))
-       
-}
+
 
 
 
 
 function cargarProductos() {
-    
+
     const cuerpo = document.getElementById("cuerpo")
-          cuerpo.innerHTML = ""
-          productos.forEach(producto => {
-            cuerpo.innerHTML += `<tr>
+    cuerpo.innerHTML = ""
+    productos.forEach(producto => {
+        cuerpo.innerHTML += `<tr>
                                     <td>${producto.nombre}</td>
                                     <td>${producto.contenidoNeto}</td>
                                     <td>${producto.precio}</td>
                                     <td>${producto.stock}</td>
                                 </tr>`
-          })
+    })
+
+
 }
 
+function guardarDatos(){
+    
+    localStorage.setItem("nombre", nombre)
+    localStorage.setItem("contenidoNeto", contenidoNeto)
+    localStorage.setItem("precio", precio)
+    localStorage.setItem("stock", stock)
+}
+
+function recuperarDatos(){
+    nombre.value = localStorage.getItem("nombre")
+    contenidoNeto.value = localStorage.getItem("contenidoNeto")
+    precio.value = localStorage.getItem("precio")
+    stock.value = localStorage.getItem("stock")
+}
